8e2887de26baae60f081cc70f2eb4730637990cf
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2023-01-04 17:02:29 +0100

    Add OutlinedPlaceholderTextField for persistent placeholder text.

--- androidApp/src/main/java/dev/avatsav/linkding/android/debug/PlaygroundActivity.kt
+++ androidApp/src/main/java/dev/avatsav/linkding/android/debug/PlaygroundActivity.kt
@@ -3,12 +3,12 @@ package dev.avatsav.linkding.android.debug
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -18,10 +18,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import dev.avatsav.linkding.android.ui.components.OutlinedPlaceholderTextField
+import dev.avatsav.linkding.android.ui.components.OutlinedTagsTextField
 import dev.avatsav.linkding.android.ui.components.Tag
-import dev.avatsav.linkding.android.ui.components.TagsTextField
 import dev.avatsav.linkding.android.ui.components.TagsTextFieldValue
 import dev.avatsav.linkding.android.ui.theme.LinkdingTheme
 
@@ -44,26 +44,21 @@ fun PlaygroundScreen() {
             Column(
                 modifier = Modifier
                     .padding(paddingValues)
-                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+                    .fillMaxSize(), verticalArrangement = Arrangement.spacedBy(36.dp)
             ) {
-                val tagsValue = remember { TagsTextFieldValue(tags = emptyList()) }
-
-                var textValue by remember { mutableStateOf(TextFieldValue()) }
-                val onValueChange: (TextFieldValue) -> Unit = { textValue = it }
-                TagsTextField(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
+                val tagsValue = remember { TagsTextFieldValue(tags = defaultTags) }
+                var text by remember { mutableStateOf("") }
+                OutlinedTagsTextField(
+                    modifier = Modifier.fillMaxWidth(),
                     value = tagsValue,
                     label = { Text(text = "Tags") },
                 )
-                OutlinedTextField(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    value = textValue,
-                    onValueChange = onValueChange,
-                    label = { Text(text = "Another") })
+                OutlinedPlaceholderTextField(modifier = Modifier.fillMaxWidth(),
+                    value = text,
+                    onValueChange = { value -> text = value },
+                    label = { Text(text = "Description") },
+                    placeholder = { Text(text = "Stories of folks reaching Staff Engineer roles.") })
             }
         }
     }
