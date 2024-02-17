860dbe85cef3d017861a5781979d33c2d6e93e2d
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2024-02-17 21:38:03 +0100

    Set default window size for desktop app.

--- app/desktop/src/jvmMain/kotlin/dev/avatsav/linkding/Main.kt
+++ app/desktop/src/jvmMain/kotlin/dev/avatsav/linkding/Main.kt
@@ -1,9 +1,14 @@
 package dev.avatsav.linkding
 
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.rememberCircuitNavigator
 import dev.avatsav.linkding.ui.RootScreen
@@ -13,8 +18,14 @@ import java.net.URI
 fun main() = application {
     val appComponent = remember { DesktopAppComponent.create() }
 
+    val windowState = rememberWindowState(
+        size = DpSize(450.dp, 900.dp),
+        position = WindowPosition(Alignment.Center),
+    )
+
     Window(
         title = "Linkding",
+        state = windowState,
         onCloseRequest = ::exitApplication,
     ) {
         val uiComponent = remember(appComponent) { DesktopUiComponent.create(appComponent) }
