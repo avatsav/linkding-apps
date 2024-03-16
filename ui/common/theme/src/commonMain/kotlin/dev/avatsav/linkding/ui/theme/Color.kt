da9ae65af3287125c99fb955e24f738d47bffd1c
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2024-03-16 17:54:46 +0100

    Apply spotless.

--- ui/common/theme/src/commonMain/kotlin/dev/avatsav/linkding/ui/theme/Color.kt
+++ ui/common/theme/src/commonMain/kotlin/dev/avatsav/linkding/ui/theme/Color.kt
@@ -64,5 +64,4 @@ val md_theme_dark_surfaceTint = Color(0xFFB8C4FF)
 val md_theme_dark_outlineVariant = Color(0xFF45464F)
 val md_theme_dark_scrim = Color(0xFF000000)
 
-
 val seed = Color(0xFF3457D5)
