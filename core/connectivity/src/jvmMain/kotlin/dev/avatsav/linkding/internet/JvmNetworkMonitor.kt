20336aa9dd603da5560383396de945e0ee4c1fdf
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2024-11-16 15:40:57 +0100

    feat(di): fix desktop build

--- core/connectivity/src/jvmMain/kotlin/dev/avatsav/linkding/internet/JvmNetworkMonitor.kt
+++ core/connectivity/src/jvmMain/kotlin/dev/avatsav/linkding/internet/JvmNetworkMonitor.kt
@@ -1,9 +1,11 @@
 package dev.avatsav.linkding.internet
 
+import me.tatarka.inject.annotations.Inject
 import software.amazon.lastmile.kotlin.inject.anvil.AppScope
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
 import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
 
+@Inject
 @SingleIn(AppScope::class)
 @ContributesBinding(AppScope::class)
 class JvmNetworkMonitor : NetworkMonitor {
