54816855c8530ad5246be94ddbf76b88be154d8f
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2025-03-17 17:54:19 +0100

    fix: add compose detekt config

--- gradle/build-logic/convention/src/main/kotlin/dev/avatsav/gradle/Detekt.kt
+++ gradle/build-logic/convention/src/main/kotlin/dev/avatsav/gradle/Detekt.kt
@@ -8,14 +8,18 @@ import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
 
 internal fun Project.configureDetekt() {
-  // Skip Spotless for thirdparty module
+  // Skip Detekt for thirdparty module
   if (path.startsWith(":thirdparty")) return
 
   pluginManager.apply("io.gitlab.arturbosch.detekt")
   configure<DetektExtension> {
-    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    config.setFrom(rootProject.files("config/detekt/detekt.yml"))
     parallel = true
   }
+
+  val detektComposeRules = libs.findLibrary("detektComposeRules").get()
+  dependencies { add("detektPlugins", detektComposeRules) }
+
   tasks.withType<Detekt>().configureEach {
     exclude { element -> element.file.path.contains("/build/generated/") }
   }
@@ -25,7 +29,4 @@ internal fun Project.configureDetekt() {
     description = "Run all detekt tasks with type resolution for Kotlin Multiplatform"
     dependsOn(tasks.withType<Detekt>())
   }
-
-  val detektComposeRules = libs.findLibrary("detektComposeRules").get().get().toString()
-  dependencies { add("detektPlugins", detektComposeRules) }
 }
