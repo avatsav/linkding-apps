59bccef7ce2b40f249af58d7ca58cbdb2720e577
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2025-08-30 22:36:03 +0200

    chore: refactor UserComponent and add platform-specific DI components for Android, iOS, and Desktop

---
+++ app/shared/src/androidMain/kotlin/dev/avatsav/linkding/inject/AndroidUserComponent.kt
@@ -0,0 +1,16 @@
+package dev.avatsav.linkding.inject
+
+import dev.avatsav.linkding.data.model.ApiConfig
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.GraphExtension
+import dev.zacsweers.metro.Provides
+
+@GraphExtension(UserScope::class)
+interface AndroidUserComponent : UserComponent {
+
+  @ContributesTo(UiScope::class)
+  @GraphExtension.Factory
+  interface Factory : UserComponent.Factory {
+    override fun create(@Provides apiConfig: ApiConfig): AndroidUserComponent
+  }
+}
--- app/shared/src/commonMain/kotlin/dev/avatsav/linkding/inject/UserComponent.kt
+++ app/shared/src/commonMain/kotlin/dev/avatsav/linkding/inject/UserComponent.kt
@@ -3,18 +3,14 @@ package dev.avatsav.linkding.inject
 import com.slack.circuit.foundation.Circuit
 import dev.avatsav.linkding.data.model.ApiConfig
 import dev.avatsav.linkding.inject.qualifier.Authenticated
-import dev.zacsweers.metro.ContributesTo
-import dev.zacsweers.metro.GraphExtension
-import dev.zacsweers.metro.Provides
+import dev.zacsweers.metro.SingleIn
 
-@GraphExtension(UserScope::class)
+@SingleIn(UserScope::class)
 interface UserComponent {
 
   @Authenticated val circuit: Circuit
 
-  @ContributesTo(UiScope::class)
-  @GraphExtension.Factory
   interface Factory {
-    fun create(@Provides apiConfig: ApiConfig): UserComponent
+    fun create(apiConfig: ApiConfig): UserComponent
   }
 }
