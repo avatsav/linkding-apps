23b463fd353e74198479fa3858ac1ac4ec4c060c
Abhijith Srivatsav <abhijith.srivatsav@gmail.com>
2026-01-27 20:54:16 +0100

    chore: fix ios platform dispatcher for molecule

--- core/presenter/src/iosMain/kotlin/dev/avatsav/linkding/presenter/MoleculeContext.kt
+++ core/presenter/src/iosMain/kotlin/dev/avatsav/linkding/presenter/MoleculeContext.kt
@@ -3,7 +3,8 @@ package dev.avatsav.linkding.presenter
 import app.cash.molecule.DisplayLinkClock
 import app.cash.molecule.RecompositionMode
 import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.Dispatchers
 
-internal actual val PlatformMainDispatcher: CoroutineContext = DisplayLinkClock
+internal actual val PlatformMainDispatcher: CoroutineContext = DisplayLinkClock + Dispatchers.Main
 
 actual val PlatformRecompositionMode: RecompositionMode = RecompositionMode.ContextClock
